0194bceff69c63252bba49381da95ef0a607c7cd
egzdeveloper <egzdeveloper@gmail.com>
2024-07-09 08:44:46 +0200

    Conexion a base de datos

--- BBDD/prueba_bd.py
+++ BBDD/prueba_bd.py
@@ -2,7 +2,7 @@ import psycopg2
 
 conexion = psycopg2.connect(
     user='postgres',
-    password='torvalds',
+    password='*******',
     host='localhost',
     port='5432',
     database='test_db'
